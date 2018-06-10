492f54def4eb3f404c3de9beb4ccac6a1517b645
yugandar <yugandar.bala@gmail.com>
2018-06-10 23:22:12 +0530

    first commit

---
+++ app/src/main/java/com/example/goa/socialmedia/GridView.java
@@ -0,0 +1,13 @@
+package com.example.goa.socialmedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view);
+    }
+}
---
+++ app/src/main/java/com/example/goa/socialmedia/MapActvity.java
@@ -0,0 +1,13 @@
+package com.example.goa.socialmedia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_actvity);
+    }
+}
